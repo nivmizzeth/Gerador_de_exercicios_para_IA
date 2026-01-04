527ba9d101a46f96e97a85ba1561a95a7f5b4ea0
Léo Marques <nivmizzeth@gmail.com>
2026-01-04 19:23:14 -0300

    regras git

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+//Quando é um repositorio recem criado
+//git init
+//git add . ou git add "nome do arquivo"
+//git commit -m "coloque uma msg"
+//git branch -M main
+//git remote add origin https://github.com/SEU_USUARIO/NOME_DO_REPOSITORIO.git
+//git push -u origin main
+
+//Depois do repositorio criado
+//git add .
+//git commit -m "msg"
+//git push
+
 function pares(numero){
     
     if (numero % 2 === 0){
