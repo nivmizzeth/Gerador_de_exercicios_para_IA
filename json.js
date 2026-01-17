733717c1c94068ca08084728ac94d4f4ff745adc
Léo Marques <nivmizzeth@gmail.com>
2026-01-17 17:51:41 -0300

    controle de veiculos

---
+++ json.js
@@ -0,0 +1,11 @@
+//nome do veiculo, total abastecido e km rodados
+let veiculos = [
+    ["Gol", 200, 120],
+    ["Corsa", 168, 100],
+    ["Palio", 170, 130],
+    ["onix", 220, 160]
+]
+
+for (i = 0; i < veiculos.length; i++) {
+    Let nomeVeiculo = 
+}
